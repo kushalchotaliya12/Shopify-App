9534b75201044622a97484b35fe14a70eb14dc12
Kushal Chotaliya <kushalv.1997@gmail.com>
2021-01-06 23:04:02 -0500

    Added products to shopify store

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,13 @@
 import { Heading, Page } from "@shopify/polaris";
 
 const Index = () => (
-  <Page>
-    <Heading>Shopify app with Node and React 🎉</Heading>
-  </Page>
+  <Page
+    title="Product"
+    primaryAction={{
+      content:'Save',
+      onAction: () => console.log('clicked')
+    }}
+  />
 );
 
 export default Index;
